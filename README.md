d5a4b1a08f4badc7dc8204765f708f547e81f099
Alex Root-Roatch <arootroatch@gmail.com>
2023-12-24 19:48:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pomodoro Clock
+
+This project was done as part of freeCodeCamp's Front End Frameworks certification. I used React, Redux, and SCSS.
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Break from './components/Break'
+import Session from './components/Session'
+import Timer from './components/Timer'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Pomodoro Clock</h1>
+      <Break/>
+      <Session/>
+      <Timer/>
+    </>
+  )
+}
+
+export default App
