96e877264d92d313b8c01b689c4c7e94ba8ec2b2
Alex Root-Roatch <arootroatch@gmail.com>
2023-12-27 03:36:12 -0600

    delete unused redux slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import breakSlice from './breakSlice';
-import sessionSlice from './sessionSlice';
 import timerSlice from './timerSlice';
 
 const store = configureStore({
     reducer: {
-        break: breakSlice,
-        session: sessionSlice,
         timer: timerSlice
     }
 })
